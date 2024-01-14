681a3d1af7611c92815d387d18592973fd26521b
Asvpxamo <151842387+Asvpxamo@users.noreply.github.com>
2024-01-14 22:38:30 +0100

    New project created

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Login from './login.svelte';   
+</script>
+
+<div class="container">
+    <div class="grid">
+
+        <h1>Welcome to my fitness app!</h1>
+
+    </div>
+    <Login />
+
+</div>
+
+<style>
+    h1 {
+        color: orangered;
+        
+    }
+    .container {
+        display: grid;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ src/routes/login.svelte
@@ -0,0 +1,36 @@
+<script>
+    
+
+</script>
+<div class="container">
+
+    <div class="grid">
+        <div class="grid-item">
+            <h1>Login Below Please</h1>
+        </div>
+        <div>
+            1
+        </div>
+        <div>
+            1
+        </div>
+    </div>
+    
+</div>
+
+<style>
+    h1 {
+        color: orange;
+    }
+    .container {
+        justify-content: center;
+        align-items: center;
+    }
+    .grid {
+        display: grid;
+        float: left;
+        position: absolute;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+        }
+</style>
